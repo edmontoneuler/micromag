0facacf0f906b300d957d3722c93191aecda5753
edmontoneuler <cameron.rudderham@gmail.com>
2020-04-16 18:54:01 -0300

    SP4 average magnetization plotter

---
+++ standard_prob4/plot_average_mag.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data = np.loadtxt('table.txt')
+
+t = data[:, 0]
+mx = data[:, 1]
+my = data[:, 2]
+mz = data[:, 3]
+
+
+plt.figure()
+
+plt.plot(t, mx, 'r')
+plt.plot(t, my, 'b')
+plt.plot(t, mz, 'g')
+plt.legend(['Mx', 'My', 'Mz'])
+
+plt.xlabel('Time (s)')
+plt.ylabel('M/Ms')
+plt.title('Average Magnetization Components')
+
+plt.show()
+
+
+
