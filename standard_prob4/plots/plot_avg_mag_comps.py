e758872d55fcd20640d2187b6b99d29d76576a6b
edmontoneuler <cameron.rudderham@gmail.com>
2020-04-29 18:34:50 -0300

    restructured SP4 folder

--- standard_prob4/plots/plot_avg_mag_comps.py
+++ standard_prob4/plots/plot_avg_mag_comps.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-data = np.loadtxt('field1_table.txt')
+data = np.loadtxt('field2_table.txt')
 
 t = data[:, 0]
 mx = data[:, 1]
@@ -13,9 +13,14 @@ mz = data[:, 3]
 plt.figure()
 
 plt.plot(t, mx, 'r')
-plt.plot(t, my, 'b')
-plt.plot(t, mz, 'g')
+plt.plot(t, my, 'g')
+plt.plot(t, mz, 'b')
 plt.legend(['Mx', 'My', 'Mz'])
+
+#Field 1 Limiting Values
+plt.axhline(-0.985964, color = 'r', linestyle = '--')
+plt.axhline(-0.172186, color = 'g', linestyle = '--')
+plt.axhline(0, color = 'b', linestyle = '--')
 
 plt.xlabel('Time (s)')
 plt.ylabel('M/Ms')
